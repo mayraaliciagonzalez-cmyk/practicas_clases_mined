110c14d822e977847a617992390a416c7b9e6e17
mayraaliciagonzalez@clases.edu.sv <mayra.alicia.gonzalez@clases.edu.sv>
2026-04-24 20:00:44 -0600

    sintaxis javascript

---
+++ javascript/main.js
@@ -0,0 +1,18 @@
+//sintaxis de javascript
+/*otro comentario*/
+
+//declaracion de variables y constantes
+//variable
+var nombre = "Juan Pérez"; //string
+let edad = 30;//int
+let decimal=20.2; //double/float
+let esCasada=false; //boolean
+
+//constante
+const PI=3.1416; //double/float
+const SALUDO="Hola Mundo"; //string
+
+//declaracion de funciones
+function saludar(nombre){
+    console.log("Hola "+nombre);
+}
